1babffc07be6265ef58eab36be8059db3b26ca02
scott1028 <mic1028002@gmail.com>
2013-12-21 19:37:18 +0800

    modified:   client.py

--- client.py
+++ client.py
@@ -1,5 +1,11 @@
 # coding:utf-8
-
+# 
+# When this module is executed the tasks will be named starting with “__main__”,
+# but when the module is imported by another process, say to call a task,
+# the tasks will be named starting with “tasks”.
+# 
+# 參考：http://docs.celeryproject.org/en/master/userguide/application.html
+#
 # worker task 必須使用引用
 from tasks import *
 
