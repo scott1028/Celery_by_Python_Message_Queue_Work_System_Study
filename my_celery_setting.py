770b8c2bd52da228f808328830452549c9d71229
scott1028 <mic1028002@gmail.com>
2014-01-02 19:55:40 +0800

    add database sqlite3 broker sample

--- my_celery_setting.py
+++ my_celery_setting.py
@@ -6,8 +6,15 @@ CELERY_ENABLE_UTC=True
 
 # Broker 的 URL, 相當於 inline 設定的 broker 屬性。
 # 參考：http://docs.celeryproject.org/en/latest/getting-started/brokers/sqlalchemy.html#broker-sqlalchemy
-BROKER_URL = 'sqla+mysql://root:t0036659@localhost/for_celery'
+# MySQL
+# BROKER_URL = 'sqla+mysql://root:t0036659@localhost/for_celery'
+# SQLite
+BROKER_URL = 'sqla+sqlite:///celerydb.sqlite'
+
 
 # 用來存放結果的 URL
 # 參考：http://docs.celeryproject.org/en/latest/configuration.html#conf-database-result-backend
-CELERY_RESULT_BACKEND = 'db+mysql://root:t0036659@localhost/for_celery'
+# MySQL
+# CELERY_RESULT_BACKEND = 'db+mysql://root:t0036659@localhost/for_celery'
+# SQLite
+CELERY_RESULT_BACKEND = 'db+sqlite:///celerydb.sqlite'
